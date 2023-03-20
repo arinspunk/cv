dcf274de0f918d152c16430ddda1a2bad0059861
Xúlio Zé <arinspunk@gmail.com>
2023-03-20 20:02:49 +0100

    fix the block title on top when the block is in the viewport

--- js/application.js
+++ js/application.js
@@ -1,40 +1,57 @@
-const siteBody = document.querySelector('.body')
-
-let totalWidth = window.innerWidth
-	header     = document.querySelector('.c-header')
-
-
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Footer Copyright Year 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-const currentDate = new Date()
-const year = currentDate.getFullYear()
 const footerYear = document.querySelector('.c-footer__year')
-footerYear.textContent += year
+footerYear.textContent += new Date().getFullYear()
 
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Detect Scroll Down/Up
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-let scrollPos = 0 // Initial state
-// adding scroll event
-window.addEventListener('scroll', function(){
-	if (window.pageYOffset>250) {
-		// detects new state and compares it with the new one
-		// Scroll up
-		if ((document.body.getBoundingClientRect()).top > scrollPos) {
-			header.classList.remove('c-header--hide')
-		}
-		// Scroll down
-		else {
-			header.classList.add('c-header--hide')
-		}
-		// saves the new position for iteration.
-		scrollPos = (document.body.getBoundingClientRect()).top
-	} else {}
-})
+// let scrollPos = 0 // Initial state
+// // adding scroll event
+// window.addEventListener('scroll', function(){
+// 	if (window.pageYOffset>250) {
+// 		// detects new state and compares it with the new one
+// 		// Scroll up
+// 		if ((document.body.getBoundingClientRect()).top > scrollPos) {
+// 			header.classList.remove('c-header--hide')
+// 		}
+// 		// Scroll down
+// 		else {
+// 			header.classList.add('c-header--hide')
+// 		}
+// 		// saves the new position for iteration.
+// 		scrollPos = (document.body.getBoundingClientRect()).top
+// 	} else {}
+// })
+
+/*
+
+Initializing the header variable at the beginning, outside the event listener, to avoid performing a DOM query every time the event is triggered.
+Destructuring the body and pageYOffset variables to make the code more concise and readable.
+Destructuring the top property from the result of getBoundingClientRect() to avoid multiple calls to that function, and storing the result in a isScrollingUp variable to make the code more readable.
+Using a ternary operator to conditionally add or remove the c-header--hide class on the header element, instead of an if...else statement.
+Removing the empty else block, since it doesn't have any effect on the behavior of the script.
+
+*/
+
+let scrollPos = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function() {
+  const { body } = document;
+  const { pageYOffset } = window;
+  const { top } = body.getBoundingClientRect();
+  const isScrollingUp = top > scrollPos;
+
+  if (pageYOffset > 250) {
+    isScrollingUp ? header.classList.remove('c-header--hide') : header.classList.add('c-header--hide');
+    scrollPos = top;
+  }
+});
 
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -59,3 +76,32 @@ document.addEventListener( 'scroll', event => {
 		else {}
 	}
 })
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Fixed titles if it block is in viewport 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// get the element you want to add the class to
+const historyBlocks = document.querySelectorAll('.c-history__block');
+
+// add a scroll event listener to the window
+window.addEventListener('scroll', function() {
+	historyBlocks.forEach(element => {
+		const child = element.querySelector('.c-history__subtitle');
+		// get the distance from the top of the viewport to the element
+		let distanceFromTop = element.getBoundingClientRect().top;
+		let distanceFromBottom = element.getBoundingClientRect().bottom;
+		// check if the element is 40px from the top of the viewport
+		if (distanceFromTop <= 60 && distanceFromBottom >= 136) {
+			// add the class to the element
+			child.classList.add('c-history__subtitle--fixed');
+			child.classList.remove('c-history__subtitle--fixed-out');
+		} else if (distanceFromTop <= 60 && distanceFromBottom >= 80){
+			child.classList.add('c-history__subtitle--fixed-out');
+		} else {
+			child.classList.remove('c-history__subtitle--fixed');
+			child.classList.remove('c-history__subtitle--fixed-out');
+		}
+	});
+});
